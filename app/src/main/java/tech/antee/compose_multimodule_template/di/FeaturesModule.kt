47c1ec9002ca1832c6aa809410ff6f45b0924ba1
Azat Gilyazov <57267956+AnteeOne@users.noreply.github.com>
2023-02-11 22:03:40 +0500

    fix: mock features map

--- app/src/main/java/tech/antee/compose_multimodule_template/di/FeaturesModule.kt
+++ app/src/main/java/tech/antee/compose_multimodule_template/di/FeaturesModule.kt
@@ -1,8 +1,17 @@
 package tech.antee.compose_multimodule_template.di
 
 import dagger.Module
+import dagger.Provides
+import tech.antee.compose_multimodule_template.multi_compose.Feature
 
 @Module(
     includes = []
 )
-interface FeaturesModule
+interface FeaturesModule {
+
+    // TODO: delete this object after implementing at least one feature
+    companion object {
+        @Provides
+        fun provideDefaultFeatures(): Map<Class<out Feature>, @JvmSuppressWildcards Feature> = emptyMap()
+    }
+}
